824fe74553245a6d6a725e75819366a159a24a68
yahirruiz18 <yahirruiz46@aragon.unam.mx>
2022-12-01 06:24:32 -0600

    se agrega el ADT sobre tablas Hash

---
+++ Tareas EDD/TablasHash/src/tablashash/HashADT.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tablashash;
+
+/**
+ *
+ * @author yahir
+ */
+public class HashADT {
+    
+    HashADT[] hashTable = new HashADT[10];
+    int dato;
+    int estado; //0 = Vacío, 1 = Eliminado, 2 = Ocupado
+    int m = 10;
+
+    static int funcion(int n, int m) 
+    {
+        return ((n + 1) % m);
+    }
+
+    static void addHash(HashADT[] h, int m, int n) {
+        boolean i = false;
+        int j = funcion(n, m);
+        do {
+            if (h[j].estado == 0 || h[j].estado == 1) {
+                h[j].dato = n;
+                h[j].estado = 2;
+                i = true;
+            } else 
+            {
+                j++;
+            }
+        } while (j < m && !i);
+        if (i)
+        {
+            javax.swing.JOptionPane.showMessageDialog(null, "elemento insertado correctamente");
+        } else 
+        {
+            javax.swing.JOptionPane.showMessageDialog(null, "Hash is full");
+        }
+    }
+
+    static int valueOfHash(HashADT[] h, int m, int n) {
+        int j = funcion(n, m);
+        while (j < m) {
+            if (h[j].estado == 0)
+            {
+                return -1;
+            } else if (h[j].dato == n) 
+            {
+                if (h[j].estado == 1)
+                {
+                    return -1;
+                } else 
+                {
+                    return j;
+                }
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+
+    static int eliminaHash(HashADT[] h, int m, int n) 
+    {
+        int i = valueOfHash(h, m, n);
+        if (i == -1) 
+        {
+            return -1;
+        } else 
+        {
+            h[i].estado = 1;
+            return 1;
+        }
+    }
+    
+}
---
+++ Tareas EDD/TablasHash/src/tablashash/TablasHash.java
@@ -0,0 +1,34 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tablashash;
+
+/**
+ *
+ * @author yahir
+ */
+public class TablasHash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        /*El ADT ya está listo, me guie de esta documentacion para realizarlo
+        
+        https://sites.google.com/a/espe.edu.ec/programacion-ii/home/tablas-hash
+        
+        tiene la misma logica pero yo le hice el cambio para que el array que ala¿macena la
+        tabla hash este implementado desde el ADT y no se haga en tiempo de programacion
+        aun debo de cambiar el uso de int por generios para que mi programa sirva en casos
+        generales y no en particulares
+        
+        
+        */
+        
+    }
+    
+}
